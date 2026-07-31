8814c79e088afffa0f1c8aad9e735291089cd8d0
Younes <66238240+younescs@users.noreply.github.com>
2026-07-30 22:16:52 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dico = {}
+        for char in s:
+            if char in dico:
+                dico[char] += 1
+            else:
+                dico[char] = 1
+
+        for char in t:
+            if char not in dico or dico[char] == 0:
+                return False
+            dico[char] -= 1
+        
+
+        for key in dico:
+            if dico[key] != 0:
+                return False
+        return True
+
+        
